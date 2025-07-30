899441c2f99705275f4a0945b70e33a1253d4b0d
Bharat Jaju <jajoo1997@gmail.com>
2025-07-30 11:57:52 +0530

    feat: Separate Supabase keys into config file

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { SUPABASE_URL, SUPABASE_KEY } from './config.js'
 
-const supabaseUrl = 'https://qlrmpymzcedwgtpxazrg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFscm1weW16Y2Vkd2d0cHhhenJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3OTE4OTgsImV4cCI6MjA2OTM2Nzg5OH0.JakI7myLl5Nt_BUmY4Tdqojp6mTc-hzA_PZIL3xwZv0'
+const supabaseUrl = SUPABASE_URL
+const supabaseKey = SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
